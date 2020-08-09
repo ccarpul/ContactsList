17ad7c938ef429dfb15779da9ce76e4564bfedc1
Carlos Pulido <carlosepulidor@gmail.com>
2020-08-09 15:48:06 -0300

    config refresh UI after touch icon favorite

--- app/src/main/java/com/example/mycontacs/utils/CollectionExtensions.kt
+++ app/src/main/java/com/example/mycontacs/utils/CollectionExtensions.kt
@@ -1,29 +1,38 @@
 package com.example.mycontacs.utils
 
-import android.util.Log
 import com.example.mycontacs.R
-import com.example.mycontacs.adapter.Header
+import com.example.mycontacs.adapter.modelAdapter.Header
 import com.example.mycontacs.data.model.ModelContactsItem
 
-fun customSort(list: List<ModelContactsItem>): List<Any> {
-    var sorted = list
-    sorted = sorted.sortedWith(compareBy { it.name })
-    sorted = sorted.sortedWith(compareByDescending { it.isFavorite })
-    val otherContacts = arrayListOf<ModelContactsItem>()
-    val favoriteContacts = arrayListOf<ModelContactsItem>()
-    sorted.forEach {
-        if (!it.isFavorite) {
-            otherContacts.add(it)
-        } else {
-            favoriteContacts.add(it)
-        }
+fun ArrayList<ModelContactsItem>.customSort(): ArrayList<*> {
+
+    val sortedList =
+        this.sortedWith(compareBy { it.name })
+        .sortedWith(compareByDescending { it.isFavorite })
+
+    return sortedList.addHeader()
+}
+
+fun List<ModelContactsItem>.addHeader(): ArrayList<Any>{
+
+    val otherContacts = arrayListOf<Any>(
+        Header(
+            R.string.title_list_Others
+        )
+    )
+    val favoriteContacts = arrayListOf<Any>(
+        Header(
+            R.string.title_list_Favorites
+        )
+    )
+
+    this.forEach {
+        if (it.isFavorite) favoriteContacts.add(it)
+        else otherContacts.add(it)
     }
 
-    val listFinal = arrayListOf<Any>()
-    listFinal.add(Header(R.string.title_list_Favorites))
-    listFinal.addAll(favoriteContacts)
-    listFinal.add(Header(R.string.title_list_Others))
-    listFinal.addAll(otherContacts)
-    return listFinal
+    favoriteContacts.addAll(otherContacts)
+
+    return favoriteContacts
 }
 
